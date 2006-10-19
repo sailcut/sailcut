a367d6984c47a89301e607266795f28e736d2adc
jeremy_laine <jeremy_laine@c3177b7d-f51e-0410-8216-fdbf5279f82f>
2006-10-19 11:36:34 +0000

    add CFormHullDef

---
+++ trunk/sailcut/src/formhulldef.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 1993-2006 Robert & Jeremy Laine
+ * See AUTHORS file for a full list of contributors.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef FORMHULLDEF_H
+#define FORMHULLDEF_H
+
+#include "formhulldefbase.h"
+#include "hulldef.h"
+
+
+/** The hull definition dialog, where the user enters
+ *  the parameters of the hull.
+ */
+class CFormHullDef : public QDialog, private Ui::CFormHullDefBase
+{
+    Q_OBJECT
+
+public:
+    CFormHullDef( QWidget *, CHullDef* );
+
+    bool check();
+    virtual void accept();
+
+protected:
+    /** a pointer to the hull definion */
+    CHullDef *hulldef;
+
+};
+
+#endif
