4355e54cdb791d9d5f2016730a4f1f06b1188cf5
Jeremy Lainé <jeremy.laine@m4x.org>
2006-10-19 11:36:34 +0000

    add CFormHullDef

---
+++ src/formhulldef.cpp
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 1993-2006 Robert & Jeremy Laine
+ * See AUTHORS file for a full list of contributors.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "formhulldef.h"
+#include <QLabel>
+#include <QMessageBox>
+#include <QRadioButton>
+#include <QLineEdit>
+
+//////////////////////////////////////////////////////////////////////
+// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+
+/** The constructor.
+ *
+ * @param parent the parent window
+ * @param hullptr pointer to the CHullDef
+ */
+CFormHullDef::CFormHullDef( QWidget* parent, CHullDef * hullptr )
+        : QDialog(parent)
+{
+    setupUi(this);
+    setModal(true);
+
+    /* we store the pointer to the CHullDef so we can update it when
+       the user clicks OK */
+    hulldef = hullptr;
+
+    txtHullID->setText(QString(hulldef->hullID));
+
+//    txtLOA->setText( QString::number(hulldef->LOA ) );
+    connect( btnOK, SIGNAL( clicked() ), this, SLOT( accept() ) );
+    connect( btnCancel, SIGNAL( clicked() ), this, SLOT( reject() ) );
+}
+
+
+// Qt overrides
+
+/** Saves the parameters entered by the user in the CHullDef.
+ *  slot connected to OK button
+ */
+void CFormHullDef::accept()
+{
+    // return data if everything is OK. /////////////////////////
+    if (check() ==true)
+        QDialog::accept();
+}
+
+
+/** Check all dimensions entered in order
+ *  to make sure that the sail is possible and reasonable
+ */
+bool CFormHullDef::check()
+{
+    hulldef->hullID = txtHullID->text();
+    ///  return true IF everything is OK
+    return true;
+}
+
+
