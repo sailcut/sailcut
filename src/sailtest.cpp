48e55fe13697306801c2f2291df73962369cfd5b
Jeremy Lainé <jeremy.laine@m4x.org>
2008-01-08 12:27:09 +0000

    add sailtest : test program for performance tests

---
+++ src/sailtest.cpp
@@ -0,0 +1,26 @@
+#include "sailworker.h"
+
+int main()
+{
+    CSailDef def;
+    CSailWorker worker(def);
+
+    // output
+    CPanelGroup sail;
+    CPanelGroup sail2;
+    CRect3d rect;
+
+    for (size_t i = 0; i < 10; i++)
+    {
+        // create sail
+        sail = worker.makeSail();
+        
+        // rotate sail
+        CMatrix rot = CMatrix::rot3d(1, PI/3);
+        sail2 = sail.rotate(1, rot);
+
+        // calculate bounding rectangle
+        rect = sail2.boundingRect();
+    }
+    return 0;
+}
