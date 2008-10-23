dfcf69e744d56c829b8e9c5ff7f8f7c3cb123def
jeremy_laine <jeremy_laine@c3177b7d-f51e-0410-8216-fdbf5279f82f>
2008-10-23 06:03:18 +0000

    simplify m4

--- trunk/sailcut/autogen.sh
+++ trunk/sailcut/autogen.sh
@@ -8,8 +8,6 @@ if test -d /opt/local/share/aclocal; then
   ACLOCAL_FLAGS=-I/opt/local/share/aclocal
 fi
 
-cat admin/utils.m4.in admin/qt.m4.in admin/sailcut.m4.in > acinclude.m4
-
 aclocal $ACLOCAL_FLAGS \
   && $LIBTOOLIZE --force --copy \
   && autoheader \
