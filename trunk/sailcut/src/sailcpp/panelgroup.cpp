30844bbad1a7fbd6e64c6f0d341077c8056da10d
jeremy_laine <jeremy_laine@c3177b7d-f51e-0410-8216-fdbf5279f82f>
2008-11-14 14:14:38 +0000

    fix build failure with gcc 4.4 (Debian bug #505369)

--- trunk/sailcut/src/sailcpp/panelgroup.cpp
+++ trunk/sailcut/src/sailcpp/panelgroup.cpp
@@ -17,6 +17,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <cstdio>
+
 #include "panelgroup.h"
 
 /** Constructs a panel group with the specified number of panels.
