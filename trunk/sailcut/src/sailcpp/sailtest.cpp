7d230e9eddd2d84979ef7565d716ab4d1888c52b
jeremy_laine <jeremy_laine@c3177b7d-f51e-0410-8216-fdbf5279f82f>
2008-11-17 21:02:45 +0000

    improve testing

--- trunk/sailcut/src/sailcpp/sailtest.cpp
+++ trunk/sailcut/src/sailcpp/sailtest.cpp
@@ -1,26 +1,72 @@
+#include <cstdio>
+
+#include <sys/time.h>
+
+#include "hullworker.h"
 #include "sailworker.h"
+
+static struct timeval start_time;
+
+static void tic()
+{
+    gettimeofday(&start_time, NULL);
+}
+
+static double toc()
+{
+  struct timeval end;
+  gettimeofday(&end, NULL);
+  return (end.tv_sec - start_time.tv_sec) + (double)(end.tv_usec - start_time.tv_usec) / 1000000;
+}
 
 int main()
 {
-    CSailDef def;
-    CSailWorker worker(def);
+    const size_t iters = 100;
+    CMatrix rot = CMatrix::rot3d(1, PI/3);
 
     // output
-    CPanelGroup sail;
-    CPanelGroup sail2;
+    CPanelGroup output, output2;
     CRect3d rect;
 
-    for (size_t i = 0; i < 10; i++)
-    {
-        // create sail
-        sail = worker.makeSail();
+    printf("Running %i iterations for each operation\n", int(iters));
 
-        // rotate sail
-        CMatrix rot = CMatrix::rot3d(1, PI/3);
-        sail2 = sail.rotate(1, rot);
+    // SAIL OPERATIONS 
+    CSailDef saildef;
+    CSailWorker sailworker(saildef);
 
-        // calculate bounding rectangle
-        rect = sail2.boundingRect();
-    }
+    // sail creation
+    tic();
+    for (size_t i = 0; i < iters; i++)
+        output = sailworker.makeSail();
+    printf("Sail creation: %lf iterations/sec\n", double(iters) / toc());
+
+    // sail rotation
+    tic();
+    for (size_t i = 0; i < iters; i++)
+        output2 = output.rotate(1, rot);
+    printf("Sail rotation: %lf iterations/sec\n", double(iters) / toc());
+
+    // bounding rectangle calculation
+    tic();
+    for (size_t i = 0; i < iters; i++)
+        rect = output2.boundingRect();
+    printf("Sail bounding rect: %lf iterations/sec\n", double(iters) / toc());
+
+    // HULL OPERATIONS 
+    CHullDef hulldef;
+    CHullWorker hullworker(hulldef);
+
+    // hull creation
+    tic();
+    for (size_t i = 0; i < iters; i++)
+        output = hullworker.makeHull();
+    printf("Hull creation: %lf iterations/sec\n", double(iters) / toc());
+
+    // hull rotation
+    tic();
+    for (size_t i = 0; i < iters; i++)
+        output2 = output.rotate(1, rot);
+    printf("Hull rotation: %lf iterations/sec\n", double(iters) / toc());
+
     return 0;
 }
