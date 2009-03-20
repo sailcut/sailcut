f20dee35328eec7e2f218f36cb55750a59b509f1
jeremy_laine <jeremy_laine@c3177b7d-f51e-0410-8216-fdbf5279f82f>
2009-03-20 17:30:08 +0000

    start generating documentation

--- trunk/sailcut/cmake/SailcutDocs.cmake
+++ trunk/sailcut/cmake/SailcutDocs.cmake
@@ -1,5 +1,8 @@
 # Macros for generating Sailcut CAD's documentation
 
+find_program(FIG2DEV fig2dev)
+find_program(XSLTPROC xsltproc)
+
 # TRANSFORM_DOCBOOK(OUTPUT INPUT..)
 macro(transform_docbook OUTPUT)
 
@@ -9,8 +12,9 @@ endmacro(transform_docbook)
 macro(transform_figs OUTPUT)
 	set(${OUTPUT})
 	foreach(THIS_FIG ${ARGN})
-		get_filename_component(_basename ${THIS_FIG} NAME_WE)
-		set(THIS_PNG ${_basename}.png)
+		get_filename_component(_fig_name ${THIS_FIG} NAME_WE)
+		get_filename_component(_fig_path ${THIS_FIG} PATH)
+		set(THIS_PNG ${_fig_path}/${_fig_name}.png)
 		add_custom_command(OUTPUT ${THIS_PNG}
 			COMMAND ${FIG2DEV} -L png -S2 ${CMAKE_CURRENT_SOURCE_DIR}/${THIS_FIG} ${THIS_PNG}
 			DEPENDS ${THIS_FIG})
